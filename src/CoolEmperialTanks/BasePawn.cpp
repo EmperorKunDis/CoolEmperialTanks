2f3c37f146f52f513997b75c10e2606beaf0dcbe
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-03-08 08:51:31 +0000

    On branch EmperorKunDis-dev-0.0.4  Your branch is up to date with 'origin/EmperorKunDis-dev-0.0.1'.
     Changes to be committed:
    	new file:   .vscode/c_cpp_properties.json
    	new file:   .vscode/launch.json
    	new file:   .vscode/settings.json
    	renamed:    Content -> src/CoolEmperialTanks.Target.cs
        new file:   src/CoolEmperialTanks/BasePawn.cpp
    	new file:   src/CoolEmperialTanks/BasePawn.h
    	new file:   src/CoolEmperialTanks/HPComponent.cpp
    	new file:   src/CoolEmperialTanks/HPComponent.h
    	new file:   src/CoolEmperrialTanksEditor.Target.cs

---
+++ src/CoolEmperialTanks/BasePawn.cpp
@@ -0,0 +1,58 @@
+#include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
+#include "Projectile.h"
+#include "Kismet/GameplayStaticsTypes.h"
+#include "Particles/ParticleSystem.h"
+
+
+ABasePawn::ABasePawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(CapsuleComp);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
+}
+
+void ABasePawn::HandleDestruction()
+{
+	if (DeathParticles)
+		UGameplayStatics::SpawnEmitterAtLocation(this, DeathParticles, GetActorLocation(), GetActorRotation());
+	if (DeathSound)
+		UGameplayStatics::PlaySoundAtLocation(this, DeathSound, GetActorLocation());
+	if (DeathCameraShakeClass)
+		GetWorld()->GetFirstPlayerController()->ClientStartCameraShake(DeathCameraShakeClass);
+	
+}
+
+void ABasePawn::RotateTurret(FVector LookAtTarget)
+{
+	FVector toTraget = LookAtTarget - TurretMesh->GetComponentLocation();
+	FRotator lookAtRotation = toTraget.Rotation();
+	lookAtRotation.Pitch = 0.0f;
+	lookAtRotation.Roll = 0.0f;
+
+	TurretMesh->SetWorldRotation(
+		FMath::RInterpTo(TurretMesh->GetComponentRotation(),
+		lookAtRotation,
+		UGameplayStatics::GetWorldDeltaSeconds(this),
+		10.0f));
+}
+
+void ABasePawn::Fire()
+{
+	AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(
+		ProjectileClass,
+		ProjectileSpawnPoint->GetComponentLocation(),
+		ProjectileSpawnPoint->GetComponentRotation());
+	Projectile->SetOwner(this);
+}
